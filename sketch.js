3767872abd787c6bd4af8f33b0f2e66e0c9355ce
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2022-08-15 17:21:04 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,26 +5,27 @@ let clearButton;
 let saveButton;
 let slider;
 let hue = 0;
-let offset = 0
+let offset = 0;
 let myCanvas;
+let colorBtn;
 
 function setup() {
   myCanvas = createCanvas(350, 350);
-  myCanvas.parent('wrapper')
+  myCanvas.parent("wrapper");
   background(0);
   angleMode(DEGREES);
-  colorMode(HSB,255,255,255)
+  colorMode(HSB, 255, 255, 255);
   saveButton = createButton("save");
-  saveButton.parent('wrapper')
+  saveButton.parent("wrapper");
   saveButton.mousePressed(saveSnowflake);
-  saveButton.addClass('saveBtn')
+  saveButton.addClass("saveBtn");
   clearButton = createButton("clear");
-  clearButton.parent('wrapper')
+  clearButton.parent("wrapper");
   clearButton.mousePressed(clearCanvas);
-  clearButton.addClass('clearBtn')
+  clearButton.addClass("clearBtn");
   slider = createSlider(1, 32, 4, 0.1);
-  slider.parent('wrapper')
-  slider.addClass('slider1');
+  slider.parent("wrapper");
+  slider.addClass("slider1");
 }
 function saveSnowflake() {
   save("snowflake.png");
@@ -32,10 +33,8 @@ function saveSnowflake() {
 
 function clearCanvas() {
   background(0);
-   window.location.reload();
-  
+  window.location.reload();
 }
-
 function draw() {
   translate(width / 2, height / 2);
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
@@ -45,8 +44,9 @@ function draw() {
     let pmy = pmouseY - height / 2;
 
     if (mouseIsPressed) {
-      hue += noise(255)
-      stroke(hue, 255,255,100);
+      hue = hue += noise(100);
+      // hue = 255
+      stroke(hue, 255, 255, 100);
       let angle = 360 / symmetry;
 
       for (let i = 0; i < symmetry; i++) {
